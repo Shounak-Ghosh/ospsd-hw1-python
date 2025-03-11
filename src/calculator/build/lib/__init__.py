662d98c30b76851f6b1680f75b58434c10f1be15
Soohyeuk <csh040816@gmail.com>
2025-03-10 22:34:04 -0400

    Initial template implementation with calculator, logger, and notifier components

---
+++ src/calculator/build/lib/__init__.py
@@ -0,0 +1,13 @@
+"""Calculator Component API
+
+This component provides basic arithmetic operations.
+"""
+
+from .calculator import add, subtract, multiply, divide
+
+__all__ = [
+    'add',
+    'subtract',
+    'multiply',
+    'divide'
+]
